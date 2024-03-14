9895a0d59a6938a839fba691e11d91630a853215
Victor Adly <42696918+victor0089@users.noreply.github.com>
2024-03-14 11:20:12 +0200

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
-'''Task 0's module.
-'''
+"""
+Module to perform addition of two float numbers.
+"""
+
 
 def add(a: float, b: float) -> float:
     """
     Adds two floats and returns their sum.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
     """
     return a + b
